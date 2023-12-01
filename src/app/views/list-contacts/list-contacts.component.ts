59283ba946b619735738f241dae6186e6e67cbb5
Ángel Alberto de los Ríos Navarro <angeldevprojects@gmail.com>
2023-12-01 14:37:45 +0100

    Browser in contacts list

--- src/app/views/list-contacts/list-contacts.component.ts
+++ src/app/views/list-contacts/list-contacts.component.ts
@@ -4,46 +4,52 @@ import { ContactService } from 'src/app/services/contact.service';
 import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
-  selector: 'app-list-contacts',
-  templateUrl: './list-contacts.component.html',
-  styleUrls: ['./list-contacts.component.css'],
+    selector: 'app-list-contacts',
+    templateUrl: './list-contacts.component.html',
+    styleUrls: ['./list-contacts.component.css'],
 })
 export class ListContactsComponent implements OnInit {
-  contacts: Contact[] = [];
+    contacts: Contact[] = [];
+    filteredContacts: Contact[] = [];
+    searchTerm: string = '';
 
-  constructor(
-    private contactService: ContactService,
-    private toastService: ToastService
-  ) {}
+    constructor(
+        private contactService: ContactService,
+        private toastService: ToastService
+    ) {}
 
-  ngOnInit(): void {
-    this.getContacts();
-  }
+    ngOnInit(): void {
+        this.getContacts();
+    }
 
-  getContacts() {
-    this.contactService.getAllContacts().subscribe(
-      (contactsObtained: Contact[]) => {
-        this.contacts = contactsObtained.map((contact: Contact) => ({
-          id: contact.id,
-          name: contact.name,
-          surname: contact.surname,
-          phoneNumber: contact.phoneNumber,
-          email: contact.email,
-        }));
+    getContacts() {
+        this.contactService.getAllContacts().subscribe(
+            (contactsObtained: Contact[]) => {
+                this.contacts = contactsObtained.map((contact: Contact) => ({
+                    id: contact.id,
+                    name: contact.name,
+                    surname: contact.surname,
+                    phoneNumber: contact.phoneNumber,
+                    email: contact.email,
+                }));
 
-        this.contacts.sort((a, b) => {
-          if (a.name < b.name) return -1;
-          if (a.name > b.name) return 1;
-          return 0;
-        });
-      },
-      (err) => {
-        console.error(err);
-        this.toastService.initiate({
-          title: 'Error',
-          content: 'Error fetching the contacts',
-        });
-      }
-    );
-  }
+                this.contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+                this.filteredContacts = [...this.contacts];
+            },
+            (err) => {
+                console.error(err);
+                this.toastService.initiate({
+                    title: 'Error',
+                    content: 'Error fetching the contacts',
+                });
+            }
+        );
+    }
+
+    filterContacts() {      
+        this.filteredContacts = this.contacts.filter((contact) =>
+            contact.name.toLowerCase().includes(this.searchTerm.toLowerCase()) || contact.surname.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
+    }
 }
